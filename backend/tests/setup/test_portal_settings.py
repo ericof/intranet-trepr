cec68d278d7c444cd9fa2a3e32d2488dd274f26a
Érico Andrei <ericof@gmail.com>
2024-10-10 11:17:21 -0300

    Exercício 17.1: Testes de backend

---
+++ backend/tests/setup/test_portal_settings.py
@@ -0,0 +1,13 @@
+"""Portal settings tests."""
+
+from plone import api
+
+
+class TestPortalSettings:
+    """Test that Portal configuration is correctly done."""
+
+    def test_portal_title(self, portal):
+        """Test portal title."""
+        value = api.portal.get_registry_record("plone.site_title")
+        expected = "Intranet do TRE-PR"
+        assert value == expected
