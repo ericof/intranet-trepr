2960cbac091174f1605530bf6cfe04af1b715e15
Érico Andrei <ericof@gmail.com>
2024-10-10 11:21:38 -0300

    Exercício 17: Testes de backend

--- backend/tests/setup/test_portal_settings.py
+++ backend/tests/setup/test_portal_settings.py
@@ -2,12 +2,22 @@
 
 from plone import api
 
+import pytest
+
 
 class TestPortalSettings:
     """Test that Portal configuration is correctly done."""
 
-    def test_portal_title(self, portal):
+    @pytest.mark.parametrize(
+        "key,expected",
+        [
+            ["plone.site_title", "Intranet do TRE-PR"],
+            ["plone.email_from_name", "Intranet do TRE-PR"],
+            ["plone.smtp_host", "localhost"],
+            ["plone.smtp_port", 25],
+        ],
+    )
+    def test_setting(self, portal, key: str, expected: str):
         """Test portal title."""
-        value = api.portal.get_registry_record("plone.site_title")
-        expected = "Intranet do TRE-PR"
+        value = api.portal.get_registry_record(key)
         assert value == expected
